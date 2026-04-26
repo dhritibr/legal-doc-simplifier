11818fcee2c5d0a68f456f3df648ae582568b8e5
dhritibr <dhritibr62@gmail.com>
2026-04-26 18:00:30 +0530

    fix uploads dir on render

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./src/config/db');
+const fs = require('fs');
+const path = require('path');
+
+// create uploads folder if it doesn't exist
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
+
+module.exports = app;
